70327d46218b1f494420e6f05fefe5b4941422e8
Ernesto Lopez <hernes45@hotmail.com>
2023-02-26 14:29:55 +0100

    enh: created folder and project structure

---
+++ Sat.Recruitment.Dom/Sat.Recruitment.Dom/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Sat.Recruitment.Dom
+{
+    public class Class1
+    {
+    }
+}
---
+++ Sat.Recruitment.Pre/Sat.Recruitment.Pre/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Sat.Recruitment.Pre
+{
+    public class Class1
+    {
+    }
+}
